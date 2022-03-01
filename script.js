1eca0071336df8639aa127687ea4ece4cd56ae3a
“morganmiller97” <adams_morgan97@icloud.com>
2022-03-01 11:42:33 -0500

    JSBasicsExercise

---
+++ script.js
@@ -0,0 +1,67 @@
+//1 
+console.log(16);
+
+//2
+/* Numeric
+String
+Boolean
+Null
+Undefined
+*/
+
+//3
+// Addition is +
+// Subtraction is -
+// Multiplication is *
+// Division is /
+// Modulus is %
+// Exponentiation is **
+
+//4
+console.log(5 % 4 ); // 1
+console.log(5 % 2); // 1
+
+//5
+console.log('I\'M MARY POPPINS, Y\'ALL');
+
+
+
+//6
+console.log("Hawk" + "Eye"); 
+
+//7 
+console.log("Morgan".length + "Miller".length); // 14
+
+//8
+console.log("Avengers: Endgame"[16]);
+
+//Breakdown
+// console.log("Avengers: Endgame".length);
+
+//BONUS
+//1
+console.log("\\");
+
+//2
+console.log((5 ** 4) - 9);
+
+//3
+console.log("I".length + "Am".length  % "Iron".length .length - "Man".length );
+
+//4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGER"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+//5
+let m = 25;
+let c = 4;
+let u = m + c;
+console.log(u);
+
+
